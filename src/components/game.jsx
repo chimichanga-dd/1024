de22622350dbd3f11f6a5518ba82df6a95e71f01
David Dixon <chimichanga@g.ucla.edu>
2020-02-27 11:42:59 -0800

    create startState for game

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -6,6 +6,21 @@ class Game extends React.Component{
 
     constructor(props){
         super(props)
+
+        this.state = this.startState()
+    }
+
+    startState(){
+        let tiles = []
+        for(let i = 0; i < 4; i++){
+            tiles[i] = [];
+            for(let j = 0; j < 4; j++){
+                tiles[i][j] = null
+            }
+        }
+        return{
+            tiles
+        }
     }
 
     render(){
