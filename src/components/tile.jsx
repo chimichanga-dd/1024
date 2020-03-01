87304553eea34b0f5e267d165016ab3c2db45ea3
David Dixon <chimichanga@g.ucla.edu>
2020-02-29 19:02:59 -0800

    add value to tile class

--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -7,7 +7,7 @@ class Tile extends React.Component{
         let x = col * (100 + 10) + 'px'
         let y = row * (100 + 10) + 'px'
         let pos = {transform: `translate3d(${x}, ${y}, 0)`}
-        return <div className="tile" style={pos} key={uid}>
+        return <div className={`tile tile-val-${tile.tile.value}`} style={pos} key={uid}>
             {tile.tile.value}
         </div>
     }
