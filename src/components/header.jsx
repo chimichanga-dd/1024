6c79f5c0f264033683ce39292b2255de5abdf874
David Dixon <chimichanga@g.ucla.edu>
2020-03-01 22:30:48 -0800

    add header component for app

---
+++ src/components/header.jsx
@@ -0,0 +1,35 @@
+
+import React from "react"
+
+class Header extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            score: null
+        }
+    }
+
+    render(){
+        let score = this.state.score
+
+        return (
+            <div className="game-header">
+                <div className="title-and-score">
+                    <h1 className="title">1024</h1>
+                    <div className="score">
+                        <h5>score</h5>
+                        <div>{score}</div>
+                    </div>
+                </div>
+                <div className="desc-and-new-game">
+                    <p>Join the numbers and get to the <strong>1024</strong> tile!</p>
+                    <button> DOES NOTHING </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Game from "./components/game"
+import Header from "./components/header"
 
+
+const App = () => {
+    return <div className="app">
+        <Header />
+        <Game />
+    </div>
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
     let gameContainer = document.getElementById("app-container");
-    ReactDOM.render(<Game />, gameContainer)
+    ReactDOM.render(<App />, gameContainer)
 })
