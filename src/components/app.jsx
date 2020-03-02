d9a89756459e15d03f31df614d7c880235b60b48
David Dixon <chimichanga@g.ucla.edu>
2020-03-02 12:26:43 -0800

    add directions

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -27,6 +27,9 @@ class App extends React.Component{
             <div className="app">
                 <Header score={this.state.score}/>
                 <Game updateScore={this.updateScore.bind(this)}/>
+                <div className="directions">
+                    <strong>HOW TO PLAY:</strong> Use your <strong>arrow keys</strong> to move the tiles. When two tiles with the same number touch, they <strong>merge into one!</strong>
+                </div>
             </div>
         )
     }
