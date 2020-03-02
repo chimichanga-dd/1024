deed7a5c60bb2d56483e6f2d1ec594fc6c4220ef
David Dixon <chimichanga@g.ucla.edu>
2020-03-01 20:23:15 -0800

    put tile values in p tag

--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -8,7 +8,7 @@ class Tile extends React.Component{
         let y = row * (100 + 10) + 'px'
         let pos = {transform: `translate3d(${x}, ${y}, 0)`}
         return <div className={`tile tile-val-${tile.tile.value}`} style={pos} key={uid}>
-            {tile.tile.value}
+            <p>{tile.tile.value}</p>
         </div>
     }
 
