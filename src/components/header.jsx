5b8cd19d56d261d1ef09b1b7b48d9b69d0b67f3f
David Dixon <chimichanga@g.ucla.edu>
2020-03-02 20:05:15 -0800

    change header to functional component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,26 +3,26 @@ import React from "react"
 
 const Header = (props) => {
 
-        let {score, bestScore} = props
+    let {score, bestScore} = props
 
-        return (
-            <div className="game-header">
-                <div className="title-and-score">
-                    <h1 className="title">1024</h1>
-                    <div className="score-container">
-                        <h5 className="score-title">score</h5>
-                        <div className="score-number">{score}</div>
-                    </div>
-                    <div className="score-container">
-                        <h5 className="score-title">best score</h5>
-                        <div className="score-number">{bestScore}</div>
-                    </div>
+    return (
+        <div className="game-header">
+            <div className="title-and-score">
+                <h1 className="title">1024</h1>
+                <div className="score-container">
+                    <h5 className="score-title">score</h5>
+                    <div className="score-number">{score}</div>
                 </div>
-                <div className="desc-and-new-game">
-                    <p>Join the numbers and get to the <strong>1024</strong> tile!</p>
+                <div className="score-container">
+                    <h5 className="score-title">best score</h5>
+                    <div className="score-number">{bestScore}</div>
                 </div>
             </div>
-        )
+            <div className="desc-and-new-game">
+                <p>Join the numbers and get to the <strong>1024</strong> tile!</p>
+            </div>
+        </div>
+    )
 }
 
 export default Header
